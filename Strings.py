1005b7efb0984190760d3fc96f58822104f4b9aa
Anderson Burgess <sjburgess7@gmail.com>
2018-06-06 16:59:38 -0400

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -21,7 +21,9 @@ print(Sport)
 
 
 #String Operations
+
 string = "My name is Anderson Burgess and I love baseball"
+
 string.upper()  # characters are now all UPPERCASE
 print(string)
 
@@ -36,6 +38,7 @@ print(string[11:27])
 
 len(string)   # Length function returns the number of characters within the string
 print(len(string))
+
 #String Formatting
 example = 'and programming.'
 print("My name is Anderson Burgess and I love baseball {}".format(example))
